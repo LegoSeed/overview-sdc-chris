62647ad47be9c9acb1d97f36c37be4727b97f3a1
JD <jd.havrilla1@gmail.com>
2021-01-29 12:02:05 -0700

    fixed some styling

--- frontend/src/components/productInfo.js
+++ frontend/src/components/productInfo.js
@@ -30,7 +30,7 @@ import Quantity from './quantity';
 const ProductInfoContainer = Styled.div`
 background-color: #FFFFFF;
 height: 95%;
-width: 80%;
+width: 90%;
 `;
 
 const Brand = Styled.div`
@@ -38,14 +38,15 @@ color: white;
 text-shadow: rgb(0, 0, 0) 4px 0px 0px, rgb(0, 0, 0) 3.875px 0.984375px 0px, rgb(0, 0, 0) 3.5px 1.90625px 0px, rgb(0, 0, 0) 2.921875px 2.71875px 0px, rgb(0, 0, 0) 2.15625px 3.359375px 0px, rgb(0, 0, 0) 1.25px 3.78125px 0px, rgb(0, 0, 0) 0.28125px 3.984375px 0px, rgb(0, 0, 0) -0.703125px 3.921875px 0px, rgb(0, 0, 0) -1.65625px 3.625px 0px, rgb(0, 0, 0) -2.5px 3.109375px 0px, rgb(0, 0, 0) -3.203125px 2.390625px 0px, rgb(0, 0, 0) -3.6875px 1.515625px 0px, rgb(0, 0, 0) -3.953125px 0.5625px 0px, rgb(0, 0, 0) -3.96875px -0.421875px 0px, rgb(0, 0, 0) -3.734375px -1.390625px 0px, rgb(0, 0, 0) -3.28125px -2.28125px 0px, rgb(0, 0, 0) -2.609375px -3.015625px 0px, rgb(0, 0, 0) -1.78125px -3.578125px 0px, rgb(0, 0, 0) -0.828125px -3.90625px 0px, rgb(0, 0, 0) 0.140625px -3.984375px 0px, rgb(0, 0, 0) 1.125px -3.828125px 0px, rgb(0, 0, 0) 2.046875px -3.421875px 0px, rgb(0, 0, 0) 2.828125px -2.8125px 0px, rgb(0, 0, 0) 3.4375px -2.03125px 0px, rgb(0, 0, 0) 3.828125px -1.109375px 0px, rgb(0, 0, 0) 3.984375px -0.125px 0px;
 text-transform: uppercase;
 font-style: oblique;
-font-size: 3.333rem;
-letter-spacing: 11px;
+font-size: 3.233rem;
+letter-spacing: 10px;
 `;
 const Name = Styled.div`
-font-size: 1.75rem;
+font-size: 1.65rem;
 font-weight: 600;
 letter-spacing: 1px;
 font-family: Arial;
+width: 90%
 `;
 const Available = Styled.div`
 margin-bottom: 15px;
